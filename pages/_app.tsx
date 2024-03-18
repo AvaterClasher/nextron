3b3cc7119efe5f51a4fe1cb47084e6bb88997aef
AvaterClasher <avater.clasher@gmail.com>
2024-03-19 02:15:35 +0530

    feat: :sparkles: Added more variations for seo purposes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,24 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
       <Head>
         <title>Nextron - Documentation made easy</title>
         <link rel='shortcut icon' href='/favicon.png' type='image/x-icon' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/site.webmanifest' />
       </Head>
       <SWRConfig
         value={{ fetcher: (url) => fetch(url).then((res) => res.json()) }}>
