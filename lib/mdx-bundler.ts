e234f74a4a57bde7b8f90489b1884b59d9efaa17
AvaterClasher <avater.clasher@gmail.com>
2024-03-20 02:32:55 +0530

    refactor: :zap: set default theme for code blocks

--- lib/mdx-bundler.ts
+++ lib/mdx-bundler.ts
@@ -4,7 +4,7 @@ import rehypeCodeTitles from 'rehype-code-titles'
 import rehypeSlug from 'rehype-slug'
 import remarkGfm from 'remark-gfm'
 import rehypePrettyCode from 'rehype-pretty-code'
-import moonlight from './moonlight-ii.json'
+// import moonlight from './moonlight-ii.json'
 import shikiThemes from './shikiThemes'
 
 const bundleMdxContent = async (rawContent: string, theme: string) => {
@@ -16,7 +16,7 @@ const bundleMdxContent = async (rawContent: string, theme: string) => {
   // }
   const syntaxHighlightingOptions = {
     // Use one of Shiki's packaged themes
-    theme: moonlight,
+    theme: 'one-dark-pro',
     onVisitLine(node: any) {
       // Prevent lines from collapsing in `display: grid` mode, and allow empty
       // lines to be copy/pasted
@@ -51,7 +51,7 @@ const bundleMdxContent = async (rawContent: string, theme: string) => {
             },
           },
         ],
-        // [rehypePrettyCode, syntaxHighlightingOptions],
+        [rehypePrettyCode, syntaxHighlightingOptions],
       ]
       return options
     },
