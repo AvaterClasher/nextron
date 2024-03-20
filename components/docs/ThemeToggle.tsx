91217c816e2bdfafe02f2316561c02f695de114f
AvaterClasher <avater.clasher@gmail.com>
2024-03-21 02:09:58 +0530

    feat: :sparkles: Add navbar to docs pages

---
+++ components/docs/ThemeToggle.tsx
@@ -0,0 +1,33 @@
+import { useTheme } from 'next-themes'
+import { useState } from 'react'
+import { Moon, Sun } from 'react-feather'
+import { MenuContent, MenuItem, MenuRoot, MenuTrigger } from '../ui/Menu'
+
+const ThemeToggle = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const { theme, setTheme } = useTheme()
+
+  return (
+    <div>
+      <MenuRoot isOpen={isOpen} setIsOpen={setIsOpen}>
+        <MenuTrigger>
+          <div className='relative -bottom-1 rounded border-none p-2 px-[8px] hover:bg-slate-50 group-focus:ring group-focus:ring-blue-500 dark:hover:bg-slate-900'>
+            {theme === 'dark' ? <Moon size={20} /> : <Sun size={20} />}
+          </div>
+        </MenuTrigger>
+        <MenuContent isOpen={isOpen}>
+          <MenuItem callBack={() => setTheme('dark')}>
+            <Moon className='relative -top-px mr-2 inline p-1' />
+            Dark
+          </MenuItem>
+          <MenuItem callBack={() => setTheme('light')}>
+            <Sun className='relative -top-px mr-2 inline p-1' />
+            Light
+          </MenuItem>
+        </MenuContent>
+      </MenuRoot>
+    </div>
+  )
+}
+
+export default ThemeToggle
---
+++ components/docs/navbar.tsx
@@ -0,0 +1,41 @@
+import { NavbarLink } from '@prisma/client'
+import Link from 'next/link'
+import { CustomLink } from '../ui/Link'
+import ThemeToggle from './ThemeToggle'
+
+const DocsNav: React.FC<{
+  links: NavbarLink[]
+  navbarCta: string
+  logo: string
+}> = ({ links, navbarCta, logo, ...props }) => {
+  return (
+    <nav className='flex w-screen items-center justify-between bg-white py-3 px-10 shadow-lg dark:border-b dark:border-b-slate-700 dark:bg-black dark:shadow-slate-800/50'>
+      <div className='text-lg font-bold'>
+        <Link href='/'>
+          <a>{logo}</a>
+        </Link>
+      </div>
+      <div className='flex items-center space-x-4'>
+        {links?.map((link) => (
+          <div key={link.id}>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href={link.link}
+              className='text-light'>
+              {link?.linkText}
+            </a>
+          </div>
+        ))}
+        <div>
+          <ThemeToggle />
+        </div>
+        <CustomLink href={navbarCta.split('|||')[1]} className='scale-90'>
+          {navbarCta.split('|||')[0]}
+        </CustomLink>
+      </div>
+    </nav>
+  )
+}
+
+export default DocsNav
