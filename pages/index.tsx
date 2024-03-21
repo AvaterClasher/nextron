5a96cb3617f83d5161b77e72c4eff4935d929009
AvaterClasher <avater.clasher@gmail.com>
2024-03-21 18:28:03 +0530

    feat: :sparkles: finish landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import { RoughNotation } from 'react-rough-notation'
 import Link from 'next/link'
 import Image from 'next/image'
 import LandingPageImage from '../public/landing-page-image.png'
-import { Fade } from 'react-awesome-reveal'
 import clsx from 'clsx'
 import {
   Zap,
@@ -13,7 +12,12 @@ import {
   Terminal,
   MessageSquare,
   GitHub,
+  Moon,
+  Box,
 } from 'react-feather'
+import WorkflowSvg from '@/components/WorkflowSvg'
+import { Markdown } from '@/components/ui/Typography'
+import { CustomLink } from '@/components/ui/Link'
 
 const Home: NextPage = () => {
   const gradient = 'bg-gradient-to-r text-transparent bg-clip-text'
@@ -25,7 +29,7 @@ const Home: NextPage = () => {
         <p className='mb-2 text-center text-sm font-semibold uppercase text-slate-500 dark:text-slate-400'>
           Open source. Free forever
         </p>
-        <h1 className='text-center text-6xl font-black md:text-7xl'>
+        <h1 className='text-center text-6xl font-black md:text-[80px]'>
           Simplest way to{' '}
           <p className='inline bg-gradient-to-r from-[#40c9ff] to-[#e81cff] bg-clip-text text-transparent'>
             create docs{' '}
@@ -44,8 +48,7 @@ const Home: NextPage = () => {
             animationDuration={2000}
             color='rgb(129 140 248)'
             animate
-            show
-          >
+            show>
             <span>auto-updating</span>
           </RoughNotation>{' '}
           <br /> documentation website up in less than 30 seconds
@@ -63,21 +66,19 @@ const Home: NextPage = () => {
         </div>
       </div>
       <div className='mt-24'>
-        <Fade direction='up' duration={500}>
-          <div className='mx-auto max-w-5xl'>
-            <Image
-              src={LandingPageImage}
-              alt=''
-              placeholder='blur'
-              className='block rounded-lg shadow-xl'
-            />
-          </div>
-        </Fade>
+        <div className='mx-auto max-w-5xl'>
+          <Image
+            src={LandingPageImage}
+            alt=''
+            placeholder='blur'
+            className='rounded-lg shadow-2xl'
+          />
+        </div>
       </div>
 
       <div className='mx-auto mt-40 mb-10 max-w-7xl px-10'>
         <div>
-          <h2 className='text-5xl font-extrabold'>
+          <h2 className='text-7xl font-extrabold'>
             A feature packed <br />
             <span className={clsx(gradient, `from-[#696eff] to-[#f8acff]`)}>
               documentation generator
@@ -92,8 +93,7 @@ const Home: NextPage = () => {
           {features.map(({ title, description, Icon }) => (
             <div
               key={title}
-              className='rounded border p-7 dark:border-slate-800'
-            >
+              className='rounded border p-7 shadow-lg dark:border-slate-800'>
               <Icon size={25} opacity={0.7} />
               <div className='mt-4'>
                 <h3 className='text-xl font-bold'>{title}</h3>
@@ -104,7 +104,113 @@ const Home: NextPage = () => {
             </div>
           ))}
         </div>
+        <div className='mt-32'>
+          <h3 className='max-w-3xl text-7xl font-extrabold'>
+            <span className='mb-2 opacity-40'>Building docs is boring.</span>
+            <br />
+            <span>
+              But Nextron makes docs{' '}
+              <span
+                className={clsx(
+                  gradient,
+                  'from-cyan-500 to-slate-100 dark:from-[#61f4de] dark:to-slate-900'
+                )}>
+                fun and intuitive
+              </span>
+            </span>
+          </h3>
+          <p className='mt-2 max-w-3xl text-lg opacity-70'>
+            Just focus on content. Nextron will take care of hosting, feedback
+            collection, blog management, SEO, lighthouse score and many more.{' '}
+            <RoughNotation
+              type='underline'
+              strokeWidth={2}
+              animationDuration={2000}
+              color='rgb(129 140 248)'
+              animate
+              show>
+              <span>Just push to GitHub and it&apos;s done.</span>
+            </RoughNotation>
+          </p>
+          <WorkflowSvg />
+        </div>
       </div>
+      <div className='w-full py-16'>
+        <div className='mx-auto max-w-7xl'>
+          <h3 className='text-7xl font-extrabold'>
+            <span className={clsx(gradient, 'from-[#1dbde6] to-[#f1515e]')}>
+              Prebuilt components{' '}
+            </span>
+            <Box
+              className='relative -top-1 inline-block'
+              strokeWidth={3}
+              size={40}
+            />
+          </h3>
+          <p className='mt-3 max-w-3xl text-lg opacity-70'>
+            <Markdown
+              text='Nextron has many prebuilt components that you can just start
+            using right away in your markdown files.'
+            />
+          </p>
+          <div className='mt-10 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5'>
+            {[
+              'Tooltips',
+              'JSX',
+              'Code playgrounds',
+              'Callouts',
+              'And much more...',
+            ].map((name) => (
+              <div key={name} className='p-4'>
+                <h3 className='flex h-full items-center justify-center rounded border bg-white px-4 py-6 text-center text-xl font-semibold text-black dark:border-slate-800 dark:bg-black dark:text-white'>
+                  {name}
+                </h3>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className='mx-auto mt-20 max-w-7xl'>
+        <h3 className=' text-7xl font-extrabold'>
+          No more{' '}
+          <span
+            className={clsx(
+              gradient,
+              'from-[#6274e7] to-[#8752a3]'
+            )}>{`{ config }`}</span>
+        </h3>
+        <p className='my-3 max-w-3xl text-lg opacity-70'>
+          Every documentation generator needs a separate <code>`.json`</code>{' '}
+          config file to function properly. Nextron works without any config.
+        </p>
+      </div>
+      <div className='text-invert mx-auto mt-20 mb-5 max-w-7xl rounded-xl py-10 shadow-xl'>
+        <h3
+          className={clsx(
+            gradient,
+            'from-[#f44369] to-[#3e3b92] text-center text-7xl font-extrabold'
+          )}>
+          Supercharge your docs now
+        </h3>
+
+        <div className='text-invert mx-auto flex max-w-xs flex-wrap justify-around'>
+          <CustomLink
+            href='/new'
+            className='mt-10 !p-4 text-center text-2xl font-semibold shadow-2xl transition-all hover:scale-105'>
+            Get started now →
+          </CustomLink>
+        </div>
+      </div>
+      <footer className='mt-10 border-t px-10 py-5 dark:border-slate-700'>
+        <div>
+          <a
+            className='text-blue-400 hover:text-blue-700'
+            rel='noopener noreferrer'
+            href='https://github.com/AvaterClasher'>
+            Made by Soumyadip Moni
+          </a>
+        </div>
+      </footer>
     </div>
   )
 }
