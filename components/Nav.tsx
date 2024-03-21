b3ac6c9c8144d7c9161972622220da2792cac6f4
AvaterClasher <avater.clasher@gmail.com>
2024-03-21 19:18:30 +0530

    feat: :sparkles: Updated the navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,9 +1,14 @@
 import Link from 'next/link'
 import Logo from './Logo'
 import { useSession } from 'next-auth/react'
+import { Menu } from 'react-feather'
+import { MenuContent, MenuItem, MenuRoot, MenuTrigger } from './ui/Menu'
+import { useState } from 'react'
 
 const Nav = () => {
   const { data, status } = useSession()
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <nav className='sticky top-0 z-50 mb-20 bg-white/70 shadow-sm backdrop-blur-md dark:bg-black/70'>
       <div className='flex items-center justify-between px-5'>
@@ -11,7 +16,26 @@ const Nav = () => {
           <Logo />
         </div>
         <div>
-          <ul className='space-x-10 py-5'>
+          <div className='block md:hidden'>
+            <MenuRoot isOpen={isOpen} setIsOpen={setIsOpen}>
+              <MenuTrigger>
+                <div>
+                  <Menu className='my-4 inline-block p-1' size={30} />
+                </div>
+                <MenuContent isOpen={isOpen}>
+                  <MenuItem link='/Nextron/docs'>Docs</MenuItem>
+                  <MenuItem link='/Nextron/blog'>Blog</MenuItem>
+                  <MenuItem link='/login'>Login</MenuItem>
+                  {status === 'unauthenticated' || status === 'loading' ? (
+                    <MenuItem link='/login'>Login</MenuItem>
+                  ) : (
+                    <MenuItem link='/dashboard'>Dashboard</MenuItem>
+                  )}
+                </MenuContent>
+              </MenuTrigger>
+            </MenuRoot>
+          </div>
+          <ul className='hidden space-x-10 py-5 md:block'>
             <Link href='/docs'>
               <a className='opacity-60 transition-all hover:opacity-100'>
                 Docs
