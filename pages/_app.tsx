62ebaf48c78b366267985863397ae1e3adaff2b4
AvaterClasher <avater.clasher@gmail.com>
2024-03-21 18:47:09 +0530

    feat: :sparkles: add seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,23 +5,44 @@ import { SessionProvider } from 'next-auth/react'
 import { SWRConfig } from 'swr'
 import Head from 'next/head'
 import { Toaster } from 'react-hot-toast'
+import { NextSeo } from 'next-seo'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Head>
-        <title>Nextron - Documentation made easy</title>
-        <link rel='shortcut icon' href='/favicon.png' type='image/x-icon' />
-      </Head>
-      <SWRConfig
-        value={{ fetcher: (url) => fetch(url).then((res) => res.json()) }}
-      >
-        <ThemeProvider attribute='class'>
-          <Component {...pageProps} />
-          <Toaster position='bottom-left' />
-        </ThemeProvider>
-      </SWRConfig>
-    </SessionProvider>
+    <div>
+      <NextSeo
+        title='Nextron'
+        description='Easiest way to build your own documentation website without any config'
+        openGraph={{
+          url: 'https://nextron.netlify.app',
+          title: 'Nextron',
+          description:
+            'Easiest way to build your own documentation website without any config',
+          images: [
+            {
+              url: 'https://nextron.netlify.app/ogimage.png',
+            },
+          ],
+        }}
+        twitter={{
+          cardType: 'summary_large_image',
+          handle: '@Avater004',
+        }}
+      />
+      <SessionProvider session={session}>
+        <Head>
+          <title>Nextron - Documentation made easy</title>
+          <link rel='shortcut icon' href='/favicon.png' type='image/x-icon' />
+        </Head>
+        <SWRConfig
+          value={{ fetcher: (url) => fetch(url).then((res) => res.json()) }}>
+          <ThemeProvider attribute='class'>
+            <Component {...pageProps} />
+            <Toaster position='bottom-left' />
+          </ThemeProvider>
+        </SWRConfig>
+      </SessionProvider>
+    </div>
   )
 }
 
