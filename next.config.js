83e790925daab7b09cf0cf36b4878566b64a2d91
AvaterClasher <avater.clasher@gmail.com>
2024-03-21 06:59:15 +0530

    feat: :sparkles: Redirect support and changed color of code block

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/support',
+        destination: 'https://github.com/AvaterClasher/nextron/issues/new',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
